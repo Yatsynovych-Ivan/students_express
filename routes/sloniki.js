66dccee0044d4f43e744bfbd7033ab5fe39b78d1
Sherepera Kateryna <katerynasherepera@gmail.com>
2026-03-23 15:14:15 +0200

    add updating || move all functions to (../controllers)

--- routes/sloniki.js
+++ routes/sloniki.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
 const router = express.Router();
 import db from '../db/connector.js';
+import { registerSlonik, deleteSlonik, updateSlonik } from '../controllers/slonikiController.js';
 
 router.get('/', async function(req, res, next) {
   const sloniki = await db.query('SELECT * FROM sloniki');
@@ -12,7 +12,6 @@ router.get('/', async function(req, res, next) {
       created_at_date: s.created_at.toLocaleDateString()
     }
   })
-
   res.render('sloniki', { sloniki: rowSloniki || [] });
 });
 
@@ -20,33 +19,8 @@ router.get('/create', async function(req, res, next) {
   res.render('forms/sloniki/sloniki_form');
 })
 
-const SALT_ROUNDS = 10
-
 router.post('/create', async function(req, res, next) {
-  console.log("Submitted data: ", req.body);
-
-
 const { username, password, age, place_of_birth } = req.body;
-
-async function registerSlonik(username, password, age, place_of_birth) {
-  try {
-    const hash = await bcrypt.hash(password, SALT_ROUNDS);
-    const query = `
-    INSERT INTO sloniki (
-          username, password_hash, age, place_of_birth
-      )
-      VALUES ($1, $2, $3, $4) 
-      RETURNING *`;
-  const res = await db.query(query, [username, hash, age, place_of_birth]);
-  
-  console.log(`✓ Slonik registered successfully: @${res.rows[0].username}`);
-  console.log('✓ Password hash stored securely.');
-  } catch (err) 
-    { console.error(err)
-      throw err;
-    // console.error(`!! Error registering slonik: this slonik: @${username} is already exist`);
-  }
-}
 
 try {
     await registerSlonik(username, password, age, place_of_birth);
@@ -62,40 +36,45 @@ router.get('/delete', async function(req, res, next) {
 })
 
 router.post('/delete', async function(req, res, next) {
-  console.log("Deleted data: ", req.body);
-  
 const { username, password} = req.body;
-
-async function deleteSlonik(username, password) {
-   try {
-      const res = await db.query('SELECT * FROM sloniki WHERE username = $1', [username]);
-      if (res.rows.length === 0) {
-         console.log('??? No slonik found with that username.');
-         return;
-      }
-      const slonik = res.rows[0];
-      const isMatch = await bcrypt.compare(password, slonik.password_hash);
-      if (isMatch) {
-         await db.query('DELETE FROM sloniki WHERE username = $1', [username]);
-         console.log(`✓  The slonik @${username} has been removed from the database..`);
-      } else {
-         res.status(403).send('Невірний пароль');
-    }
-   } catch (err) {
-      res.status(500).send(`Error during deleting`);
-   }
-}
-
 
 try {
     await deleteSlonik(username, password);
-    
     res.redirect('/sloniki');
   } catch (err) {
-    res.status(500).send(`!! Error deleting slonik: @${username}`);
+    if (err.message === 'Invalid password') {
+      res.status(403).send('Invalid password');
+    } else {
+      res.status(500).send(`!! Error deleting slonik: @${username}`);
+    }
   }
 });
 
+
+
+router.get('/update', (req, res) => {
+  res.render('forms/sloniki/updateSloniki'); 
+});
+
+router.post('/update', async (req, res) => {
+  const { currentUsername, password, newUsername, newAge, new_place_of_birth } = req.body;
+  const updateData = {
+    username: newUsername,
+    age: newAge,
+    place_of_birth: new_place_of_birth
+  };
+
+  try {
+    await updateSlonik(currentUsername, password, updateData);
+    res.redirect('/sloniki');
+  } catch (err) {
+    if (err.message === 'Invalid password') {
+      res.status(403).send('Error: invalid password');
+    } else {
+      res.status(500).send('Error updating');
+    }
+  }
+});
 
 
 export default router;
