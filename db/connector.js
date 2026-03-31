f9a61b9c4b0f3bdf3dd9294e3c9e00b07320e56f
Sherepera Kateryna <katerynasherepera@gmail.com>
2026-03-31 22:33:13 +0300

    add table SLONIKI creation

--- db/connector.js
+++ db/connector.js
@@ -7,6 +7,27 @@ const pool = new Pool({
     connectionString: process.env.DB_URL,
 });
 
-//Add table creation
+const createTableQueries = [];
+
+createTableQueries.push(`
+ CREATE TABLE IF NOT EXISTS SLONIKI (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    age TEXT NOT NULL,
+    place_of_birth TEXT NOT NULL,           
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+   );
+      `);
+for await (const query of createTableQueries) {
+    try {
+        console.log(query.slice(0, query.indexOf('(')).trim()+"...")
+        await pool.query(query);
+    } catch(err) {
+        console.error("query execution error: ", err.message);
+    }
+}
+
+console.log("CONNECTED!!!!!✅ ")
 
 export default pool;
